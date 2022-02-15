d9c1895cbfda31dfbfccb186bd458bb3a8f7bf83
LauraAlejandra2021 <3450@holbertonschool.com>
2022-02-14 23:21:24 -0500

    update 0

--- 0-script.js
+++ 0-script.js
@@ -1,3 +1,3 @@
 //change text color of header element
-header = document.querySelector("header")
-style.color = #FF0000
+const header = document.querySelector("header");
+header.style.color = "#FF0000";
